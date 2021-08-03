bf5c6b523185f0472eaa7b0d46aef38e0b5197f1
Evgenia Kurineva <kurineva85@mail.ru>
2021-08-03 22:17:12 +0300

    feat: build project

--- src/components/Card.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Card = () => <h1 className="container">Card</h1>;
-
-export default Card;
--- src/components/SearchBar.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const SearchBar = () => <h1 className="container">SearchBar</h1>;
-
-export default SearchBar;
