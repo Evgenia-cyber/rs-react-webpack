ffb357c70db07b351ea91df9f78f9851b8bb3d28
Evgenia Kurineva <kurineva85@mail.ru>
2021-08-09 17:22:50 +0300

    fix: fix html and css for card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,18 +3,20 @@ import { Link } from 'react-router-dom';
 
 const Card = ({ author, title, description, publishedAt, source, url, urlToImage }) => (
   <div className="card-container">
-    <img className="img" src={urlToImage} alt="" />
-    <p className="card-info">
-      <span className="card-info-label">Author: </span>
-      {author}
-    </p>
-    <p className="card-info">
-      <span className="card-info-label">Source: </span>
-      {source}
-    </p>
-    <p className="card-info card-date">{publishedAt}</p>
-    <h2 className="card-title">{title}</h2>
-    <p>{description}</p>
+    <div>
+      <img className="img" src={urlToImage} alt="" />
+      <p className="card-info">
+        <span className="card-info-label">Author: </span>
+        {author}
+      </p>
+      <p className="card-info">
+        <span className="card-info-label">Source: </span>
+        {source}
+      </p>
+      <p className="card-info card-date">{publishedAt}</p>
+      <h2 className="card-title">{title}</h2>
+      <p>{description}</p>
+    </div>
     <Link to={{ pathname: url }} target="_blank" rel="noopener noreferrer">
       <span className="more-btn">More info</span>
     </Link>
