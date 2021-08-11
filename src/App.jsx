15fcb0b9cd6e4402e80d821296431c72353e83e1
Evgenia Kurineva <kurineva85@mail.ru>
2021-08-11 21:04:28 +0300

    feat: add NotFound page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
+import { Route, Switch, useLocation } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Header from './components/Header';
-import RouteWithAnimation from './components/RouteWithAnimation';
 import About from './pages/About';
 import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
-const App = () => (
-  <div className="app-container">
-    <Header />
-    <RouteWithAnimation path="/">
-      <Home />
-    </RouteWithAnimation>
-    <RouteWithAnimation path="/about">
-      <About />
-    </RouteWithAnimation>
-  </div>
-);
+const App = () => {
+  const location = useLocation();
+
+  return (
+    <div className="app-container">
+      <Header />
+      <TransitionGroup>
+        <CSSTransition
+          classNames="page-animation"
+          key={location.pathname}
+          timeout={{
+            enter: 500,
+            exit: 350,
+          }}
+        >
+          <div className="page-animation">
+            <Switch location={location}>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+              <Route path="*" component={NotFound} />
+            </Switch>
+          </div>
+        </CSSTransition>
+      </TransitionGroup>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div className="about">
+    <p>No such page exists :(</p>
+  </div>
+);
+
+export default NotFound;
