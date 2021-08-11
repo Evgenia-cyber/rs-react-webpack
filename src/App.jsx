a36237f3da06f517fc6e32c1cd70efb2755c3b47
Evgenia Kurineva <kurineva85@mail.ru>
2021-08-11 18:33:15 +0300

    refactor: move animation and route into a separate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,18 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-import { CSSTransition } from 'react-transition-group';
 import Header from './components/Header';
+import RouteWithAnimation from './components/RouteWithAnimation';
 import About from './pages/About';
 import Home from './pages/Home';
 
 const App = () => (
   <div className="app-container">
     <Header />
-    <Route exact path="/">
-      {({ match }) => (
-        <CSSTransition classNames="page-animation" in={match != null} timeout={{ enter: 500, exit: 350 }} unmountOnExit>
-          <div className="page-animation">
-            <Home />
-          </div>
-        </CSSTransition>
-      )}
-    </Route>
-    <Route exact path="/about">
-      {({ match }) => (
-        <CSSTransition classNames="page-animation" in={match != null} timeout={{ enter: 500, exit: 350 }} unmountOnExit>
-          <div className="page-animation">
-            <About />
-          </div>
-        </CSSTransition>
-      )}
-    </Route>
+    <RouteWithAnimation path="/">
+      <Home />
+    </RouteWithAnimation>
+    <RouteWithAnimation path="/about">
+      <About />
+    </RouteWithAnimation>
   </div>
 );
 
